3e231c9a930d347e11e75c4690fc1b0aea84a744
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2019-12-15 14:42:17 +0800

    Implement Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,12 @@
 module Main where
 
+import Data.ByteString.Lazy.Char8 as B
+
+import Lib
+
 main :: IO ()
-main = putStrLn "main not implemented"
+main = do
+  inputBytes <- B.getContents
+  case processInputBytes inputBytes of
+    Right outputBytes -> B.putStrLn outputBytes
+    Left errorString -> Prelude.putStrLn errorString
