87da581de1dd11f9f3d4f4976a0fa3fd56ac0850
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2019-12-16 11:18:55 +0800

    Refactor to smaller functions

--- src/Contribute.hs
+++ src/Contribute.hs
@@ -12,24 +12,39 @@ import qualified OutputSchema as O
 
 contribute :: [Bill] -> [District] -> [O.Contribution]
 contribute allBills allDistricts = fmap contribution allBills where
-  contribution bill = O.Contribution { O.billName = billName bill, O.funds = fmap (fund bill) allDistricts }
-  fund bill district = O.Fund { O.district = districtName district, O.amount = min (provided district) (share (contributionProportion district) (amount bill)) } where
-    contributionProportion district = ratio (provided district) totalProvidedFunds
-    totalProvidedFunds = Prelude.foldr add (Amount 0) (fmap provided allDistricts)
-    provided district = case billCap bill (ratio billAllocated totalCategoryAllocated) district of
-                          Just capped -> min uncapped capped
-                          Nothing -> uncapped
-      where
-        uncapped = min billAllocated (billAvailableFunds bill (ratio billAllocated totalAllocated) district)
-        billAllocated = billAllocation bill district
-        totalAllocated = Prelude.foldr add (Amount 0) [billAllocation b district | b <- allBills]
-        totalCategoryAllocated = Prelude.foldr add (Amount 0) [billAllocation b district | b <- allBills, (category b) == (category bill)]
+  contribution bill = O.Contribution { O.billName = billName bill,
+                                       O.funds = fmap (fund districtProvided totalProvidedFunds requiredFunds) allDistricts } where
+    totalProvidedFunds = Prelude.foldr add (Amount 0) (fmap districtProvided allDistricts)
+    districtProvided = billProvided (totalAllocated allBills) (totalCategoryAllocated (category bill) allBills) bill
+    requiredFunds = amount bill
+
+fund :: (District -> Amount) -> Amount -> Amount -> District -> O.Fund
+fund districtProvided totalProvidedFunds requiredFunds district =
+  O.Fund { O.district = districtName district,
+           O.amount = min districtProvidedFunds (share contributionProportion requiredFunds) } where
+  contributionProportion = ratio districtProvidedFunds totalProvidedFunds
+  districtProvidedFunds = districtProvided district
+
+billProvided :: (District -> Amount) -> (District -> Amount) -> Bill -> District -> Amount
+billProvided totalAllocated totalCategoryAllocated bill district =
+  case billCap bill (ratio billAllocated (totalCategoryAllocated district)) district of
+    Just capped -> min uncapped capped
+    Nothing -> uncapped
+  where
+    uncapped = min billAllocated (billAvailableFunds (ratio billAllocated (totalAllocated district)) district)
+    billAllocated = billAllocation bill district
+
+totalAllocated :: [Bill] -> District -> Amount
+totalAllocated bills district = Prelude.foldr add (Amount 0) [billAllocation b district | b <- bills]
+
+totalCategoryAllocated :: Category -> [Bill] -> District -> Amount
+totalCategoryAllocated category bills district = Prelude.foldr add (Amount 0) [billAllocation b district | b <- bills, (Model.category b) == category]
 
 billCap :: Bill -> Rational -> District -> Maybe Amount
 billCap bill ratio district =  Map.lookup (category bill) (caps district) >>= return . share ratio
 
-billAvailableFunds :: Bill -> Rational -> District -> Amount
-billAvailableFunds bill ratio district = share ratio (availableFunds district)
+billAvailableFunds :: Rational -> District -> Amount
+billAvailableFunds ratio district = share ratio (availableFunds district)
 
 share :: Rational -> Amount -> Amount
 share ratio (Amount total) = Amount $ (numerator ratio) * total `div` (denominator ratio)
